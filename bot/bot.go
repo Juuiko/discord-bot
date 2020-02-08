3aa2f4e712cc00f52ff8e25d8818d225f14729bf
Alex Mahon <mahonal@tcd.ie>
2020-02-08 00:38:09 +0000

    Added infix calculator

--- bot/bot.go
+++ bot/bot.go
@@ -1,15 +1,16 @@
 package bot
 
 import (
-   "../config"
-   "fmt"
-   "github.com/bwmarrin/discordgo"
-   "strings"
-   "math/rand"
-   "time"
-   "io/ioutil"
-   "net/http"
-   "strconv"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"../config"
+	"github.com/bwmarrin/discordgo"
 )
 
 var BotID string
@@ -18,215 +19,227 @@ var vcCon *discordgo.VoiceConnection
 
 var ConnectionMap map[string]int64
 
-func userGoodbye(s *discordgo.Session, u *discordgo.GuildMemberRemove){
-   _, _ = s.ChannelMessageSend(WelcomeChannel,  fmt.Sprintf("%s was banned by the tyranical Crassus, Fs in chat pls <:OBKick:643516408994594817> :cry:", u.User.Username))
-   return
+func userGoodbye(s *discordgo.Session, u *discordgo.GuildMemberRemove) {
+	_, _ = s.ChannelMessageSend(WelcomeChannel, fmt.Sprintf("%s was banned by the tyranical Crassus, Fs in chat pls <:OBKick:643516408994594817> :cry:", u.User.Username))
+	return
 }
 
-func userWelcome(s *discordgo.Session, u *discordgo.GuildMemberAdd){
-   _, _ = s.ChannelMessageSend(WelcomeChannel, fmt.Sprintf("Hey %s, welcome to **Quantex Esports Network** :tada: :hugging: <:OBKiss:643520085197062164> !", u.User.Mention()))
-   return
+func userWelcome(s *discordgo.Session, u *discordgo.GuildMemberAdd) {
+	_, _ = s.ChannelMessageSend(WelcomeChannel, fmt.Sprintf("Hey %s, welcome to **Quantex Esports Network** :tada: :hugging: <:OBKiss:643520085197062164> !", u.User.Mention()))
+	return
 }
 
-func logHandler(s *discordgo.Session, m *discordgo.MessageCreate){
-   if m.Author.ID == BotID {
-      return
-   }
-   if strings.Contains(m.Content, "@") {m.Content = strings.Replace(m.Content, "@", "[at]", 10)}
-   _, _ = s.ChannelMessageSend(LogsChannel, fmt.Sprintf("\"%s\" - %s in %s", m.Content, m.Author.String(), ChannelNameByID[m.ChannelID]))
+func logHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotID {
+		return
+	}
+	if strings.Contains(m.Content, "@") {
+		m.Content = strings.Replace(m.Content, "@", "[at]", 10)
+	}
+	_, _ = s.ChannelMessageSend(LogsChannel, fmt.Sprintf("\"%s\" - %s in %s", m.Content, m.Author.String(), ChannelNameByID[m.ChannelID]))
 }
 
-func voiceHandler(s *discordgo.Session, u *discordgo.VoiceStateUpdate){
-   _, inMap := ConnectionMap[u.UserID]
-   if u.UserID == BotID {
-      return
-   } else if u.ChannelID == "" || u.Suppress || u.SelfMute || u.SelfDeaf || u.Mute || u.Deaf {
-     if !inMap {
-       return
-     }
-     addTimeToDB(time.Now().Unix()-ConnectionMap[u.UserID], u)
-     delete(ConnectionMap, u.UserID)
-   } else {
-     if !inMap {
-       ConnectionMap[u.UserID] = time.Now().Unix()
-     }
-   }
+func voiceHandler(s *discordgo.Session, u *discordgo.VoiceStateUpdate) {
+	_, inMap := ConnectionMap[u.UserID]
+	if u.UserID == BotID {
+		return
+	} else if u.ChannelID == "" || u.Suppress || u.SelfMute || u.SelfDeaf || u.Mute || u.Deaf {
+		if !inMap {
+			return
+		}
+		addTimeToDB(time.Now().Unix()-ConnectionMap[u.UserID], u)
+		delete(ConnectionMap, u.UserID)
+	} else {
+		if !inMap {
+			ConnectionMap[u.UserID] = time.Now().Unix()
+		}
+	}
 }
 
-func secsToHours(secs int)string{
-  var str1, str2 string
-  mins := secs / 60
-  hours := mins / 60
-  minsLeftover := mins % 60
-  if hours == 1 {
-    str1 = fmt.Sprintf("%dhr ", hours)
-  } else {
-    str1 = fmt.Sprintf("%dhrs ", hours)
-  }
-  if minsLeftover == 1 {
-    str2 = fmt.Sprintf("%dmin", minsLeftover)
-  } else {
-    str2 = fmt.Sprintf("%dmins", minsLeftover)
-  }
-  str := str1 + str2
-  return str
+func secsToHours(secs int) string {
+	var str1, str2 string
+	mins := secs / 60
+	hours := mins / 60
+	minsLeftover := mins % 60
+	if hours == 1 {
+		str1 = fmt.Sprintf("%dhr ", hours)
+	} else {
+		str1 = fmt.Sprintf("%dhrs ", hours)
+	}
+	if minsLeftover == 1 {
+		str2 = fmt.Sprintf("%dmin", minsLeftover)
+	} else {
+		str2 = fmt.Sprintf("%dmins", minsLeftover)
+	}
+	str := str1 + str2
+	return str
 }
 
-func profileEmbed(s *discordgo.Session, m *discordgo.MessageCreate){
-   mE := new(discordgo.MessageEmbed)
-   mE.Title = fmt.Sprintf("%s's profile",m.Author.Username)
+func profileEmbed(s *discordgo.Session, m *discordgo.MessageCreate) {
+	mE := new(discordgo.MessageEmbed)
+	mE.Title = fmt.Sprintf("%s's profile", m.Author.Username)
 
-   pic := new(discordgo.MessageEmbedImage)
-   pic.URL = m.Author.AvatarURL("128")
-   mE.Image = pic
-   mE.Color = 9693630
-   exp, vexp := findExp(m)
-   pos := findPos(m,exp)
-   member, err := s.GuildMember(QuantexID,m.Author.ID)
-   if err != nil {
-    fmt.Println(err.Error())
-   }
-   time, err := member.JoinedAt.Parse()
-   if err != nil {
-    fmt.Println(err.Error())
-   }
-   mE.Description = "Server exp = " + strconv.Itoa(exp) + "\nServer rank = " + strconv.Itoa(pos) + "\nVC time = " + secsToHours(vexp) + "\nJoin date = " + time.Format("02/01/2006 15:04")
-   _, err = s.ChannelMessageSendEmbed(BotCommandsChannel,mE)
-   if err != nil {
-    fmt.Println(err.Error())
-   }
+	pic := new(discordgo.MessageEmbedImage)
+	pic.URL = m.Author.AvatarURL("128")
+	mE.Image = pic
+	mE.Color = 9693630
+	exp, vexp := findExp(m)
+	pos := findPos(m, exp)
+	member, err := s.GuildMember(QuantexID, m.Author.ID)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	time, err := member.JoinedAt.Parse()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	mE.Description = "Server exp = " + strconv.Itoa(exp) + "\nServer rank = " + strconv.Itoa(pos) + "\nVC time = " + secsToHours(vexp) + "\nJoin date = " + time.Format("02/01/2006 15:04")
+	_, err = s.ChannelMessageSendEmbed(BotCommandsChannel, mE)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
 
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate){
-   if m.Author.ID == BotID {
-      return
-   }
-   addExp(m)
-   if strings.HasPrefix(m.Content, "!insecure") {
-     _, _ = s.ChannelMessageSend(m.ChannelID, "https://www.youtube.com/watch?v=4PG_elEG7rA")
-   } else if m.ChannelID == BotTestChannel {
-      if strings.HasPrefix(m.Content, config.BotPrefix) {
-         switch m.Content {
-         case "!listUsers":
-            memberListArray, _ := s.GuildMembers(QuantexID,"90530967382417408", 1000)
-            userList := "Total user list:\n"
-            for i := 0; i<len(memberListArray); i++ {
-               userList = userList + memberListArray[i].User.Username + "\n"
-            }
-            _, _ = s.ChannelMessageSend(BotTestChannel, userList)
-         case "!countUsers":
-            memberListArray, _ := s.GuildMembers(QuantexID,"0", 1000)
-            _, _ = s.ChannelMessageSend(BotTestChannel, fmt.Sprintf("Number of users: %d", strconv.Itoa(len(memberListArray)) ))
-         case "!updateList":
-            memberListArray, _ := s.GuildMembers(QuantexID,"0",1000)
-            fillDB(memberListArray)
-         }
-      }
-   } else if m.ChannelID == BotCommandsChannel || m.ChannelID == BotTestChannel {
-      if strings.HasPrefix(m.Content, config.BotPrefix) {
-         switch m.Content {
-         case "!help":
-            _, _ = s.ChannelMessageSend(BotCommandsChannel, "Command list: cointoss, ping, inspire, join, exit, top, me")
-         case "!cointoss":
-            command_cointoss(s,m)
-         case "!top":
-            printLeaderboard(s,m)
-        case "!me":
-            profileEmbed(s,m)
-         case "!ping":
-            _, _ = s.ChannelMessageSend(BotCommandsChannel, "pong")
-         case "!emoteT":
-            _, _ = s.ChannelMessageSend(BotCommandsChannel, "<:OBKiss:643520085197062164>")
-         case "!inspire":
-            command_inspire(s,m)
-         case "!join":
-            vcCon, _ = s.ChannelVoiceJoin(QuantexID,"591762237857726484",false,false)
-         case "!exit":
-            vcCon.Disconnect()
-         }
-      }
-   }
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotID {
+		return
+	}
+	addExp(m)
+	if strings.HasPrefix(m.Content, "!insecure") {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "https://www.youtube.com/watch?v=4PG_elEG7rA")
+	} else if m.ChannelID == BotTestChannel {
+		if strings.HasPrefix(m.Content, config.BotPrefix) {
+			switch m.Content {
+			case "!listUsers":
+				memberListArray, _ := s.GuildMembers(QuantexID, "90530967382417408", 1000)
+				userList := "Total user list:\n"
+				for i := 0; i < len(memberListArray); i++ {
+					userList = userList + memberListArray[i].User.Username + "\n"
+				}
+				_, _ = s.ChannelMessageSend(BotTestChannel, userList)
+			case "!countUsers":
+				memberListArray, _ := s.GuildMembers(QuantexID, "0", 1000)
+				_, _ = s.ChannelMessageSend(BotTestChannel, fmt.Sprintf("Number of users: %s", strconv.Itoa(len(memberListArray))))
+			case "!updateList":
+				memberListArray, _ := s.GuildMembers(QuantexID, "0", 1000)
+				fillDB(memberListArray)
+			}
+		}
+	} else if m.ChannelID == BotCommandsChannel || m.ChannelID == BotTestChannel {
+		if strings.HasPrefix(m.Content, config.BotPrefix) {
+			if strings.HasPrefix(m.Content, "!calc") {
+				result := strings.Split(m.Content, " ")
+				output, err := calculate(result)
+				if err != nil {
+					_, _ = s.ChannelMessageSend(BotCommandsChannel, fmt.Sprintf(err.Error()))
+				} else {
+					_, _ = s.ChannelMessageSend(BotCommandsChannel, fmt.Sprintf("Answer -> %d", output))
+				}
+			} else {
+				switch m.Content {
+				case "!help":
+					_, _ = s.ChannelMessageSend(BotCommandsChannel, "Command list: cointoss, ping, inspire, join, exit, top, calc, insecure, me")
+				case "!cointoss":
+					command_cointoss(s, m)
+				case "!top":
+					printLeaderboard(s, m)
+				case "!me":
+					profileEmbed(s, m)
+				case "!ping":
+					_, _ = s.ChannelMessageSend(BotCommandsChannel, "pong")
+				case "!emoteT":
+					_, _ = s.ChannelMessageSend(BotCommandsChannel, "<:OBKiss:643520085197062164>")
+				case "!inspire":
+					commandInspire(s, m)
+				case "!join":
+					vcCon, _ = s.ChannelVoiceJoin(QuantexID, "591762237857726484", false, false)
+				case "!exit":
+					vcCon.Disconnect()
+				}
+			}
+		}
+	}
 }
 
-   func command_inspire(s *discordgo.Session, m *discordgo.MessageCreate){
-      url := "https://inspirobot.me/api?generate=true"
-      resp, err := http.Get(url)
-      if err != nil {
-		  fmt.Println(err.Error())
-	   }
-      defer resp.Body.Close()
-      html, err := ioutil.ReadAll(resp.Body)
-	     if err != nil {
-		    fmt.Println(err.Error())
-	   }
-      _, _ = s.ChannelMessageSend(m.ChannelID, string(html))
-   }
+func commandInspire(s *discordgo.Session, m *discordgo.MessageCreate) {
+	url := "https://inspirobot.me/api?generate=true"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer resp.Body.Close()
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	_, _ = s.ChannelMessageSend(m.ChannelID, string(html))
+}
 
-   func command_cointoss(s *discordgo.Session, m *discordgo.MessageCreate){
-      coin := []string{
-              "heads",
-              "tails",
-      }
-      rand.Seed(time.Now().UnixNano())
-      side := coin[rand.Intn(len(coin))]
-      _, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("The coin landed on %s!", side))
-   }
+func command_cointoss(s *discordgo.Session, m *discordgo.MessageCreate) {
+	coin := []string{
+		"heads",
+		"tails",
+	}
+	rand.Seed(time.Now().UnixNano())
+	side := coin[rand.Intn(len(coin))]
+	_, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("The coin landed on %s!", side))
+}
 
-   func messageReactionAdd(s *discordgo.Session, r *discordgo.MessageReactionAdd){
-     if r.MessageReaction.MessageID == "674756079002976268" {
-       if r.MessageReaction.Emoji.ID == "674659046468354057"{  //EU4
-         s.GuildMemberRoleAdd(QuantexID, r.UserID, "669343691655348236")
-       }
-       if r.MessageReaction.Emoji.ID == "674659075560308766"{ //LoL
-         s.GuildMemberRoleAdd(QuantexID, r.UserID, "669344064780632175")
-       }
-       if r.MessageReaction.Emoji.ID == "674659105578811402"{  //Minecraft
-         s.GuildMemberRoleAdd(QuantexID, r.UserID, "669344100100603916")
-       }
-     }
-   }
+func messageReactionAdd(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
+	if r.MessageReaction.MessageID == "674756079002976268" {
+		if r.MessageReaction.Emoji.ID == "674659046468354057" { //EU4
+			s.GuildMemberRoleAdd(QuantexID, r.UserID, "669343691655348236")
+		}
+		if r.MessageReaction.Emoji.ID == "674659075560308766" { //LoL
+			s.GuildMemberRoleAdd(QuantexID, r.UserID, "669344064780632175")
+		}
+		if r.MessageReaction.Emoji.ID == "674659105578811402" { //Minecraft
+			s.GuildMemberRoleAdd(QuantexID, r.UserID, "669344100100603916")
+		}
+	}
+}
 
-   func messageReactionDel(s *discordgo.Session, r *discordgo.MessageReactionRemove){
-     if r.MessageReaction.MessageID == "674756079002976268" {
-       if r.MessageReaction.Emoji.ID == "674659046468354057"{
-         s.GuildMemberRoleRemove(QuantexID, r.UserID, "669343691655348236")
-       }
-       if r.MessageReaction.Emoji.ID == "674659075560308766"{
-         s.GuildMemberRoleRemove(QuantexID, r.UserID, "669344064780632175")
-       }
-       if r.MessageReaction.Emoji.ID == "674659105578811402"{
-         s.GuildMemberRoleRemove(QuantexID, r.UserID, "669344100100603916")
-       }
-     }
-   }
+func messageReactionDel(s *discordgo.Session, r *discordgo.MessageReactionRemove) {
+	if r.MessageReaction.MessageID == "674756079002976268" {
+		if r.MessageReaction.Emoji.ID == "674659046468354057" {
+			s.GuildMemberRoleRemove(QuantexID, r.UserID, "669343691655348236")
+		}
+		if r.MessageReaction.Emoji.ID == "674659075560308766" {
+			s.GuildMemberRoleRemove(QuantexID, r.UserID, "669344064780632175")
+		}
+		if r.MessageReaction.Emoji.ID == "674659105578811402" {
+			s.GuildMemberRoleRemove(QuantexID, r.UserID, "669344100100603916")
+		}
+	}
+}
 
 func Start() {
-   goBot , err := discordgo.New("Bot " + config.Token)
-   if err!= nil {
-      fmt.Println(err.Error())
-      return
-   }
+	goBot, err := discordgo.New("Bot " + config.Token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-   u, err := goBot.User("@me")
-   if err!= nil {
-      fmt.Println(err.Error())
-   }
+	u, err := goBot.User("@me")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-   BotID = u.ID
-   goBot.AddHandler(userGoodbye)
-   goBot.AddHandler(userWelcome)
-   goBot.AddHandler(logHandler)
-   goBot.AddHandler(messageHandler)
-   goBot.AddHandler(voiceHandler)
-   goBot.AddHandler(messageReactionAdd)
-   goBot.AddHandler(messageReactionDel)
+	BotID = u.ID
+	goBot.AddHandler(userGoodbye)
+	goBot.AddHandler(userWelcome)
+	goBot.AddHandler(logHandler)
+	goBot.AddHandler(messageHandler)
+	goBot.AddHandler(voiceHandler)
+	goBot.AddHandler(messageReactionAdd)
+	goBot.AddHandler(messageReactionDel)
 
-   err = goBot.Open()
-   if err!= nil {
-      fmt.Println(err.Error())
-      return
-   }
+	err = goBot.Open()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-   ConnectionMap = make(map[string]int64)
-   fmt.Println("Bot is running!")
+	ConnectionMap = make(map[string]int64)
+	fmt.Println("Bot is running!")
 }
---
+++ bot/calc.go
@@ -0,0 +1,90 @@
+package bot
+
+import (
+	"errors"
+	"strconv"
+)
+
+func hasPrecedence(op1 string, op2 string) bool {
+	if op2 == "(" || op2 == ")" {
+		return false
+	}
+	if (op1 == "*" || op1 == "/") && (op2 == "+" || op2 == "-") {
+		return false
+	}
+	return true
+}
+
+func applyOp(op string, b int, a int) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return b, errors.New("cannot divide by 0")
+		}
+		return a / b, nil
+	}
+	return 0, nil
+}
+
+func calculate(tokens []string) (int, error) {
+	var valueST []int
+	var opST []string
+	for i := 1; i <= len(tokens)-1; i++ {
+		if tokens[i] == "(" {
+			opST = append(opST, tokens[i])
+		} else if tokens[i] == ")" {
+			for opST[len(opST)-1] != "(" {
+				pop1 := opST[len(opST)-1]
+				pop2 := valueST[len(valueST)-1]
+				pop3 := valueST[len(valueST)-2]
+				opST = opST[:len(opST)-1]          // Pop opST
+				valueST = valueST[:len(valueST)-2] //Pop valuesST x2
+				newValue, err := applyOp(pop1, pop2, pop3)
+				if err != nil {
+					return 0, err
+				}
+				valueST = append(valueST, newValue)
+			}
+			opST = opST[:len(opST)-1]
+		} else if tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/" {
+			for len(opST) > 0 && hasPrecedence(tokens[i], opST[len(opST)-1]) {
+				pop1 := opST[len(opST)-1]
+				pop2 := valueST[len(valueST)-1]
+				pop3 := valueST[len(valueST)-2]
+				opST = opST[:len(opST)-1]          // Pop opST
+				valueST = valueST[:len(valueST)-2] //Pop valuesST x2
+				newValue, err := applyOp(pop1, pop2, pop3)
+				if err != nil {
+					return 0, err
+				}
+				valueST = append(valueST, newValue)
+			}
+			opST = append(opST, tokens[i])
+		} else {
+			integer, err := strconv.Atoi(tokens[i])
+			if err != nil {
+				return 0, errors.New("input error")
+			}
+			valueST = append(valueST, integer)
+		}
+	}
+	for len(opST) > 0 {
+		pop1 := opST[len(opST)-1]
+		pop2 := valueST[len(valueST)-1]
+		pop3 := valueST[len(valueST)-2]
+		opST = opST[:len(opST)-1]          // Pop opST
+		valueST = valueST[:len(valueST)-2] //Pop valuesST x2
+		newValue, err := applyOp(pop1, pop2, pop3)
+		if err != nil {
+			return 0, err
+		}
+		valueST = append(valueST, newValue)
+	}
+	return valueST[len(valueST)-1], nil
+}

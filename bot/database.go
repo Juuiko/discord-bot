a2ce7a5fc3e57799ec669ba9e29d0dbefe241ce3
Alex Mahon <mahonal@tcd.ie>
2020-03-06 23:10:11 +0000

    Added weeklys & monthlys, added auto new users

--- bot/database.go
+++ bot/database.go
@@ -15,6 +15,10 @@ type user struct {
 	discrim string
 	vexp    int
 	exp     int
+	wvexp    int
+	wexp     int
+	mvexp    int
+	mexp     int
 }
 
 var DB sql.DB = openDB()
@@ -29,29 +33,36 @@ func openDB() sql.DB {
 
 func fillDB(ml []*discordgo.Member) {
 	for i := 0; i < len(ml); i++ {
-		ID := ml[i].User.ID
-		UN := ml[i].User.Username
-		Disc := ml[i].User.Discriminator
-		sqlStmt, err := DB.Prepare("INSERT INTO users (id, name, discrim, exp, vexp) values (?, ?, ?, '0', '0');")
+		sqlStmt, err := DB.Prepare("INSERT INTO users (id, name, discrim, exp, vexp, wexp, wvexp, mexp, mvexp) values (?, ?, ?, '0', '0', '0', '0', '0', '0');")
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-		sqlStmt.Exec(ID, UN, Disc)
+		sqlStmt.Exec(ml[i].User.ID, ml[i].User.Username, ml[i].User.Discriminator)
 	}
+}
+
+func addNewUser(u *discordgo.User)  {
+	sqlStmt, err := DB.Prepare("INSERT INTO users (id, name, discrim, exp, vexp, wexp, wvexp, mexp, mvexp) values (?, ?, ?, '0', '0', '0', '0', '0', '0');")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	sqlStmt.Exec(u.ID, u.Username, u.Discriminator)
 }
 
 func addExp(m *discordgo.MessageCreate) {
 	u := new(user)
-	err := DB.QueryRow("SELECT * FROM users WHERE id = ?;", m.Author.ID).Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp)
+	err := DB.QueryRow("SELECT * FROM users WHERE id = ?;", m.Author.ID).Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp, &u.wexp, &u.wvexp, &u.mexp, &u.mvexp)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	sqlStmt, err := DB.Prepare("UPDATE users SET exp = ? WHERE id = ?;")
+	sqlStmt, err := DB.Prepare("UPDATE users SET exp = ?, wexp = ?, mexp = ? WHERE id = ?;")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	u.exp = u.exp + 10
-	sqlStmt.Exec(u.exp, m.Author.ID)
+	u.wexp = u.wexp + 10
+	u.mexp = u.mexp + 10
+	sqlStmt.Exec(u.exp, u.wexp, u.mexp, m.Author.ID)
 }
 
 func printLeaderboard(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -60,7 +71,7 @@ func printLeaderboard(s *discordgo.Session, m *discordgo.MessageCreate) {
 	rows, _ := DB.Query("SELECT * FROM users ORDER BY exp DESC LIMIT 10;")
 	defer rows.Close()
 	for rows.Next() {
-		rows.Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp)
+		rows.Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp, &u.wexp, &u.wvexp, &u.mexp, &u.mvexp)
 		message = message + u.name + ": " + strconv.Itoa(u.exp) + "\n"
 	}
 	message = message + "\n```"
@@ -73,7 +84,7 @@ func printVCLeaderboard(s *discordgo.Session, m *discordgo.MessageCreate) {
 	rows, _ := DB.Query("SELECT * FROM users ORDER BY vexp DESC LIMIT 10;")
 	defer rows.Close()
 	for rows.Next() {
-		rows.Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp)
+		rows.Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp, &u.wexp, &u.wvexp, &u.mexp, &u.mvexp)
 		message = message + u.name + ": " + secsToHours(u.vexp) + "\n"
 	}
 	message = message + "\n```"
@@ -82,7 +93,7 @@ func printVCLeaderboard(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 func findExp(m *discordgo.MessageCreate) (int, int) {
 	u := new(user)
-	err := DB.QueryRow("SELECT * FROM users WHERE id = ?;", m.Author.ID).Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp)
+	err := DB.QueryRow("SELECT * FROM users WHERE id = ?;", m.Author.ID).Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp, &u.wexp, &u.wvexp, &u.mexp, &u.mvexp)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -109,14 +120,16 @@ func findVCPos(m *discordgo.MessageCreate, vexp int) int {
 
 func addTimeToDB(time int64, m *discordgo.VoiceStateUpdate) {
 	u := new(user)
-	err := DB.QueryRow("SELECT * FROM users WHERE id = ?;", m.UserID).Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp)
+	err := DB.QueryRow("SELECT * FROM users WHERE id = ?;", m.UserID).Scan(&u.id, &u.name, &u.discrim, &u.exp, &u.vexp, &u.wexp, &u.wvexp, &u.mexp, &u.mvexp)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	sqlStmt, err := DB.Prepare("UPDATE users SET vexp = ? WHERE id = ?;")
+	sqlStmt, err := DB.Prepare("UPDATE users SET vexp = ?, wvexp = ?, mvexp = ? WHERE id = ?;")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	time = time + int64(u.vexp)
-	sqlStmt.Exec(time, m.UserID)
+	dailyTime := time + int64(u.vexp)
+	weeklyTime := time + int64(u.wvexp)
+	monthlyTime := time + int64(u.mvexp)
+	sqlStmt.Exec(dailyTime, weeklyTime, monthlyTime, m.UserID)
 }
